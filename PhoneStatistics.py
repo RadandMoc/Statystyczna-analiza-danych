47d7e1d673327d02872050f043046f5800b38311
tzapart <tomasz.t.zapart@gmail.com>
2023-12-06 09:16:47 +0100

    Make some function
    Make some function

--- PhoneStatistics.py
+++ PhoneStatistics.py
@@ -3,7 +3,32 @@ import numpy as np
 
 print("Hello World")
 
-#readData=pd.read_csv("DaneTelefonow.csv",sep=";")
+readData=pd.read_csv("DaneTelefonow.csv",sep=";")
 #readData=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
-numpy_array2 = readData.iloc[:,1:].to_numpy()
+numpy_array2 = readData.iloc[:,1:].astype(float).to_numpy()
 print(numpy_array2)
+
+
+
+def getOptimalObject(array):
+    return np.max(array,0)
+
+def getMeansArray(array):
+    return np.mean(array,0)
+    
+def getStandardDeviationArray(array):
+    return np.std(array,0)
+
+def GetStandarizatedArray(array,meanArray,stdArray):
+    return (array-meanArray)/stdArray
+    
+meanArray = getMeansArray(numpy_array2)
+stdArray = getStandardDeviationArray(numpy_array2)
+
+GetStandarizatedArray(numpy_array2,meanArray,stdArray)
+
+    
+    
+
+    
+    
