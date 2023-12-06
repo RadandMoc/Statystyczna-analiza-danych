b9cfe33032289769bfcd093c73977e888672652a
tzapart <tomasz.t.zapart@gmail.com>
2023-12-06 08:57:10 +0100

    df to nupmy
    df to nupmy

--- PhoneStatistics.py
+++ PhoneStatistics.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
 
-readData=pd.read_csv("DaneTelefonow.csv",sep=";")
-print(readData)
+print("Hello World")
+
+readData=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
+numpy_array2 = readData.iloc[:,1:].to_numpy()
+print(numpy_array2)
