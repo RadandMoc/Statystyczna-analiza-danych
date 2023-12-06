51073c898a34d562cf2688de5419c89f531b6e75
Radosław Mocarski <rmocarski@softguard.pl>
2023-12-06 08:54:53 +0100

    relative sciezka

--- PhoneStatistics.py
+++ PhoneStatistics.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
 
-print("Hello World")
-
-kot=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
-print(kot)
+readData=pd.read_csv("DaneTelefonow.csv",sep=";")
+print(readData)
