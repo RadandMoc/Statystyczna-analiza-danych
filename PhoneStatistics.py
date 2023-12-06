7788885c581f85d5ddee3696080ee2827b68120d
Radosław Mocarski <rmocarski@softguard.pl>
2023-12-06 09:21:43 +0100

    nie jest tak zle. moglo byc gorzej

--- PhoneStatistics.py
+++ PhoneStatistics.py
@@ -1,13 +1,29 @@
 import pandas as pd
 import numpy as np
 
-print("Hello World")
+def ChangeVariablesToStimulants(data,numbersOfDestimulants):
+    if not isinstance(data, np.ndarray):
+        raise ValueError("Podane dane nie są typu NumPy array")
+    
+    if not isinstance(numbersOfDestimulants, list):
+        raise ValueError("Numer kolumn musi być w postaci listy")
+    
+    num_columns = data.shape[1]
+    for col_num in numbersOfDestimulants:
+        if col_num >= num_columns:
+            raise ValueError(f"Kolumna numer {col_num} nie istnieje w danych")
+        
+    data[:, numbersOfDestimulants] *= -1
+    return data
 
-#readData=pd.read_csv("DaneTelefonow.csv",sep=";")
-readData=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
+readData=pd.read_csv("DaneTelefonow.csv",sep=";")
+#readData=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
 numpy_array2 = readData.iloc[:,1:].astype(float).to_numpy()
-print(numpy_array2)
 
+print(numpy_array2)
+numpy_array2 = ChangeVariablesToStimulants(numpy_array2,[1,4,7])
+print('A to zmienione dane')
+print(numpy_array2)
 
 
 def getOptimalObject(array):
