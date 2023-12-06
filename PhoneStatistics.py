820be020972a43f11a8c04e18337658f73bc7a4c
tzapart <tomasz.t.zapart@gmail.com>
2023-12-06 09:02:05 +0100

    update name
    update name

--- PhoneStatistics.py
+++ PhoneStatistics.py
@@ -3,6 +3,7 @@ import numpy as np
 
 print("Hello World")
 
-readData=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
+#readData=pd.read_csv("DaneTelefonow.csv",sep=";")
+#readData=pd.read_csv("C:/Users/zapar/Python/BOT/Statystyczna-analiza-danych/DaneTelefonow.csv",sep=";")
 numpy_array2 = readData.iloc[:,1:].to_numpy()
 print(numpy_array2)
