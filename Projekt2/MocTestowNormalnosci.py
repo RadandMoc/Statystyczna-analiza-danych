d28530495dc72b105f7850e2f8d03a7ac418409c
tzapart <tomasz.t.zapart@gmail.com>
2024-01-19 15:31:38 +0100

    dodanie telefonow
    dodanie telefonow

--- Projekt2/MocTestowNormalnosci.py
+++ Projekt2/MocTestowNormalnosci.py
@@ -10,6 +10,7 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 from scipy.stats import f_oneway
 import random
+import seaborn as sns
 
 class Normality_test(Enum):
     Shapiro_Wilk = "Shapiro-Wilk"
@@ -62,7 +63,7 @@ class logNormal:
 def check_test_power(normality_test,data):
     if normality_test == Normality_test.Shapiro_Wilk:
         statistic, p_value = stats.shapiro(data)
-        return p_value < 0.05
+        return p_value
     elif normality_test == Normality_test.Anderson_Darling:
         result = stats.anderson(data)
         return result.statistic > result.critical_values[2]
@@ -125,42 +126,57 @@ def t_student_distribution_power_test(list_number_of_datas, list_of_degrees_of_f
 
 
 def gamma_distribution_power_test(list_number_of_datas,shape,scale,iterations = 10000):
+    results = []
     for n in list_number_of_datas:
         for sh in shape:
             for sc in scale:
                 ts = [gammaFunction(n,sh,sc).data for _ in range(iterations)]
-                power_shapiro_wilk = 1 - (sum(check_test_power(Normality_test.Shapiro_Wilk, data) for data in ts) / iterations)
-                power_anderson_darling = 1- (sum(check_test_power(Normality_test.Anderson_Darling, data) for data in ts) / iterations)
-                power_lilliefors = 1-(sum(check_test_power(Normality_test.Lilliefors, data) for data in ts) / iterations)
-                power_jarque_bera = 1-(sum(check_test_power(Normality_test.Jarque_Bera, data) for data in ts) / iterations)
+                power_shapiro_wilk = sum(check_test_power(Normality_test.Shapiro_Wilk, data) for data in ts) / iterations
+                power_anderson_darling = sum(check_test_power(Normality_test.Anderson_Darling, data) for data in ts) / iterations
+                power_lilliefors = sum(check_test_power(Normality_test.Lilliefors, data) for data in ts) / iterations
+                power_jarque_bera = sum(check_test_power(Normality_test.Jarque_Bera, data) for data in ts) / iterations
 
                 print(f"Moc testu Shapiro-Wilka dla {n} danych o shape {sh}:", power_shapiro_wilk)
                 print(f"Moc testu Anderson_Darling dla {n} danych o shape {sh}:", power_anderson_darling)
                 print(f"Moc testu Lilliefors dla {n} danych o shape {sh}", power_lilliefors)
                 print(f"Moc testu Jarque_Bera dla {n} danych o shape {sh}", power_jarque_bera)
+                results.append([n,sh, sc, power_shapiro_wilk, power_anderson_darling, power_lilliefors, power_jarque_bera])
+    df = pd.DataFrame(results, columns=['Liczba danych', 'Ksztalt' ,'Skala', 'Shapiro-Wilk', 'Anderson-Darling', 'Lilliefors','Jarque-Bera'])
+    return df
 
 
 
-def plot_test_powers_by_sample_size(df,text):
-    std_devs = df[text].unique()
-    tests = df.columns[2:]
-   
+
+def plot_gamma_test_powers_combined(df):
+    """
+    Funkcja do rysowania wykresów mocy testów normalności dla różnych kombinacji parametrów kształtu i skali rozkładu gamma.
+    df: DataFrame zawierający wyniki funkcji gamma_distribution_power_test
+    """
+    shape_values = df['Ksztalt'].unique()
+    scale_values = df['Skala'].unique()
+    tests = df.columns[4:]  # Zakładając, że kolumny z testami zaczynają się od piątej kolumny
+
+    for shape in shape_values:
+        for scale in scale_values:
+            subset = df[(df['Ksztalt'] == shape) & (df['Skala'] == scale)]
+            if not subset.empty:
+                plt.figure(figsize=(12, 8))
+
+                for test in tests:
+                    plt.plot(subset['Liczba danych'], subset[test], label=test, marker='o')
+
+                plt.xlabel('Liczba danych')
+                plt.ylabel('Moc testu')
+                plt.xticks(subset['Liczba danych'])  # Zapewnia, że osi x odpowiadają liczbie danych
+                plt.title(f'Moc różnych testów normalności dla kształtu = {shape} i skali = {scale}')
+                plt.legend()
+                plt.grid(True)
+                plt.show()
 
 
-    for sd in std_devs:
-        plt.figure(figsize=(12, 8))
-        
-        for test in tests:
-            subset = df[df[text] == sd]
-            plt.plot(subset['Liczba danych'], subset[test], label=test)
-        
-        plt.xlabel('Liczba danych w próbce')
-        plt.ylabel('Moc testu gdy dane pochodzą z rozkładu lognormalnego')
-        plt.xticks(subset['Liczba danych'])  # Ensure x-ticks represent sample sizes
 
-        plt.title(f'Moc różnych testów normalności dla {text} = {sd}')
-        plt.legend()
-        plt.show()
+
+
 
 def cut_data_to_the_same_size(data):
     numbers_of_datas = list(range(7))
@@ -183,12 +199,19 @@ def cut_data_to_the_same_size(data):
     return returner
 
 number_of_data = [10] + list(range(25,1000,25))
-print(number_of_data)
+
 std = [1,3,5,10,20,50]
 logstd = [1/16,1/8,1/4,1/2,1,3/2,5,10]
 list_of_degrees_of_freedom = [2,5,10,50,100,500,1000,5000]
-shape = [1,2,5,10,20]
+shape = [1,2,5,10,20,50]
 scale = [1,2,4,8,12]
+scale = [12]
+
+
+
+
+#df_results = gamma_distribution_power_test(number_of_data, shape, scale)
+#plot_gamma_test_powers_combined(df_results)
 
 
 #normal = normal_distribution_power_test(number_of_data, std, True)
@@ -220,21 +243,70 @@ print(plot_test_powers_by_sample_size(lognormal,"Odchylenie"))
 
 #ANOVA
 DATA = pd.read_csv("DaneMalaIloscKolorow.csv",sep=";", decimal=",")
-#dane = DATA[DATA['Telefon\zmienne'].str.startswith("Apple")]
-#daneSAMSUNG = DATA[DATA['Telefon\zmienne'].str.startswith("SAMSUNG")]["Cena"]
-#daneHuawei = DATA[DATA['Marka'] == "Huawei"]["Cena"]
-#Czarny = DATA[DATA['Kolor'] == "Lawendowy"]["Cena"]
+black = DATA[DATA['Kolor'] == "Czarny"]["Cena"]
+blue = DATA[DATA['Kolor'] == "Niebieski"]["Cena"]
+white = DATA[DATA['Kolor'] == "Bialy"]["Cena"]
+silver = DATA[DATA['Kolor'] == "Srebrny"]["Cena"]
+yellow = DATA[DATA['Kolor'] == "Zolty"]["Cena"]
+green = DATA[DATA['Kolor'] == "Zielony"]["Cena"]
+red = DATA[DATA['Kolor'] == "Czerwony"]["Cena"]
 
-#print(check_test_power(Normality_test.Jarque_Bera,dane["Cena"]))
-#print(check_test_power(Normality_test.Jarque_Bera,daneSAMSUNG))
-#print(check_test_power(Normality_test.Jarque_Bera,daneHuawei))
+
+print(len(yellow))
+print(len(white))
+print(len(blue))
+print(len(green))
+print(len(silver))
+print(len(black))
+print(len(red))
+
+print(check_test_power(Normality_test.Shapiro_Wilk,black))
+print(check_test_power(Normality_test.Shapiro_Wilk,blue))
+print(check_test_power(Normality_test.Shapiro_Wilk,white))
+print(check_test_power(Normality_test.Shapiro_Wilk,silver))
+print(check_test_power(Normality_test.Shapiro_Wilk,yellow))
+print(check_test_power(Normality_test.Shapiro_Wilk,red))
+
+print(check_test_power(Normality_test.Shapiro_Wilk,green))
+
+
+
+
+
+Apple = DATA[DATA['Marka'] == "Apple"]["Cena"]
+Samsung = DATA[DATA['Marka'] == "Samsung"]["Cena"]
+Huawei = DATA[DATA['Marka'] == "Huawei"]["Cena"]
+Xiaomi = DATA[DATA['Marka'] == "Xiaomi"]["Cena"]
+
+print(len(Apple))
+print(len(Samsung))
+print(len(Huawei))
+print(len(Xiaomi))
+
+
+print(check_test_power(Normality_test.Shapiro_Wilk,Apple))
+print(check_test_power(Normality_test.Shapiro_Wilk,Samsung))
+print(check_test_power(Normality_test.Shapiro_Wilk,Huawei))
+print(check_test_power(Normality_test.Shapiro_Wilk,Xiaomi))
+
+
+
 
 
 #print(check_test_power(Normality_test.Jarque_Bera,Czarny))
 
-#stat, p = bartlett(dane, daneSAMSUNG, daneHuawei)
-#print(f"Nie ma podstaw do odrzucenia hipotezy zerowej p-value >0.05 {p>0.05} wartosc wynosi {p}")
+stat, p = bartlett(black, blue, white, silver, yellow)
+print(f"Nie ma podstaw do odrzucenia hipotezy zerowej p-value >0.05 {p>0.05} wartosc wynosi {p}")
 
+
+model = ols('Cena ~ Kolor', data=DATA).fit()
+anova_result = sm.stats.anova_lm(model, typ=1)
+print(anova_result)
+
+
+model2 = ols('Cena ~ Marka', data=DATA).fit()
+anova_result2 = sm.stats.anova_lm(model2, typ=1)
+print(anova_result2)
 
 #stat, p = levene(dane, daneSAMSUNG, daneHuawei)
 
